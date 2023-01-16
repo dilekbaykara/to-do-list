a0aadc9ff70dff537a2928ef4338a1c59bc22578
Dilek Baykara <baykara.d@gmail.com>
2023-01-16 18:53:42 -0500

    changed App.tsx and added to-do-list elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 interface ToDo {
-  title: string
-  priority: "normal" | "important"
-  description: string
-  checked: boolean
-  duedate: Date
+  title: string;
+  priority: "normal" | "important";
+  description: string;
+  checked: boolean;
+  duedate: Date;
 }
 
 const myToDo1: ToDo = {
   checked: true,
   description: "Take out the trash",
-  duedate: new Date(2023,1,30),
-  priority: 'important',
-  title: "Chores"
-  }
+  duedate: new Date(2023, 1, 30),
+  priority: "important",
+  title: "Chores",
+};
 
-  const myToDo2: ToDo = {
-    checked: false,
-    description: "Throw away paper",
-    duedate: new Date(2023,1,31),
-    priority: 'normal',
-    title: "Chores"
-    }
+const myToDo2: ToDo = {
+  checked: false,
+  description: "Throw away paper",
+  duedate: new Date(2023, 1, 31),
+  priority: "normal",
+  title: "Chores",
+};
 
-function ToDoItem(props:{toDo:ToDo}){
-return <div>
-  <input type="checkbox" checked = {props.toDo.checked}/>
-  <span>{props.toDo.description}</span>
-  <br/>
-  <span>{props.toDo.duedate.toLocaleDateString()}</span>
-</div>
+function ToDoItem(props: { toDo: ToDo }) {
+  return (
+    <div>
+      <input type="checkbox" checked={props.toDo.checked} />
+      <span>{props.toDo.description}</span>
+      <br />
+      <span>{props.toDo.duedate.toLocaleDateString()}</span>
+    </div>
+  );
 }
-
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
@@ -42,9 +43,7 @@ function Example() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
@@ -53,9 +52,9 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <h1>To-Do-List</h1>
-      <Example/>
-      <ToDoItem toDo={myToDo1}/>
-      <ToDoItem toDo={myToDo2}/>
+      <Example />
+      <ToDoItem toDo={myToDo1} />
+      <ToDoItem toDo={myToDo2} />
     </div>
   );
 }
