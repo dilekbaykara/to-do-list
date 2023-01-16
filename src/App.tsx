03bb1b69ae75862bc280076524b15efa60d1a591
Dilek Baykara <baykara.d@gmail.com>
2023-01-16 18:41:36 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+interface ToDo {
+  title: string
+  priority: "normal" | "important"
+  description: string
+  checked: boolean
+  duedate: Date
+}
+
+const myToDo1: ToDo = {
+  checked: true,
+  description: "Take out the trash",
+  duedate: new Date(2023,1,30),
+  priority: 'important',
+  title: "Chores"
+  }
+
+  const myToDo2: ToDo = {
+    checked: false,
+    description: "Throw away paper",
+    duedate: new Date(2023,1,31),
+    priority: 'normal',
+    title: "Chores"
+    }
+
+function ToDoItem(props:{toDo:ToDo}){
+return <div>
+  <input type="checkbox" checked = {props.toDo.checked}/>
+  <span>{props.toDo.description}</span>
+  <br/>
+  <span>{props.toDo.duedate.toLocaleDateString()}</span>
+</div>
+}
+
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do-List</h1>
+      <Example/>
+      <ToDoItem toDo={myToDo1}/>
+      <ToDoItem toDo={myToDo2}/>
     </div>
   );
 }
